0fc836009ccd854dfee0068e3b5df277b76cdda0
KillerJim-DVG <JamesHunter-DeepVoodoo@users.noreply.github.com>
2021-01-19 15:06:22 +0000

    Initial Commit

---
+++ copyPlugin.bat
@@ -0,0 +1,4 @@
+copy /Y D:\DVG\Perforce\KJModProject\Plugins\KillerJimMod\Binaries\Win64\UE4Editor-KillerJimModProxy.dll G:\DVG\KJModP_Framework\Plugins\KillerJimMod\Binaries\Win64\UE4Editor-KillerJimModProxy.dll
+copy /Y D:\DVG\Perforce\KJModProject\Plugins\KJModEditor\Binaries\Win64\UE4Editor-KJModEditor.dll G:\DVG\KJModP_Framework\Plugins\KJModEditor\Binaries\Win64\UE4Editor-KJModEditor.dll
+copy /Y D:\DVG\Perforce\KJModProject\Plugins\BlueprintJson\Binaries\Win64\UE4Editor-BlueprintJson.dll G:\DVG\KJModP_Framework\Plugins\BlueprintJson\Binaries\Win64\UE4Editor-BlueprintJson.dll
+timeout 2
